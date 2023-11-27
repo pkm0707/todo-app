f8d5fce0f4b69609dceb73e9912283803ea23bf4
pkm0707 <mpraveenkumar@student.tce.edu>
2023-11-27 07:54:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,153 +1,189 @@
 import './App.css';
-import React, { useRef, useState } from 'react'
-import EditIcon from '@mui/icons-material/Edit';
+import React, {useState,useEffect} from 'react'
+import AddTaskIcon from '@mui/icons-material/AddTask';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { IconButton } from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+// import { TextField } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
+// import { IconButton } from '@mui/material';
 
-function App() {
-  const list = [
-    {
-      id: 1, 
-      name: "Rocky",
-      discription : "Meeting with Andrews on dec 1",
-      status : "Completed"
-    },
-    {
-      id: 2, 
-      name: "Iron Man",
-      discription : "Make a perfect suit for spider man",
-      status :"Not Completed"
+export function App () {
+  const [allTodos, setAllTodos] = useState ([]);
+  const [newTodoTitle, setNewTodoTitle] = useState ('');
+  const [newDescription, setNewDescription] = useState ('');
+  const [completedTodos, setCompletedTodos] = useState ([]);
+  const [isCompletedScreen, setIsCompletedScreen] = useState (false);
+
+  const handleAddNewToDo = () => {
+    let newToDoObj = {
+      title: newTodoTitle,
+      description: newDescription,
+    };
+    // console.log (newToDoObj);
+    let updatedTodoArr = [...allTodos];
+    updatedTodoArr.push (newToDoObj);
+    // console.log (updatedTodoArr);
+    setAllTodos (updatedTodoArr);
+    localStorage.setItem ('todolist', JSON.stringify (updatedTodoArr));
+    setNewDescription ('');
+    setNewTodoTitle ('');
+  };
+
+  useEffect (() => {
+    let savedTodos = JSON.parse (localStorage.getItem ('todolist'));
+    let savedCompletedToDos = JSON.parse (
+      localStorage.getItem ('completedTodos')
+    );
+    if (savedTodos) {
+      setAllTodos (savedTodos);
     }
-  ]
-  const [lists, setList] = useState(list)
-  const [updateState, setUpdateState] = useState(-1)
+
+    if (savedCompletedToDos) {
+      setCompletedTodos (savedCompletedToDos);
+    }
+  }, []);
+
+  const handleToDoDelete = index => {
+    let reducedTodos = [...allTodos];
+    reducedTodos.splice (index,1);
+    // console.log (index);
+
+    // console.log (reducedTodos);
+    localStorage.setItem ('todolist', JSON.stringify (reducedTodos));
+    setAllTodos (reducedTodos);
+  };
+
+  const handleCompletedTodoDelete = index => {
+    let reducedCompletedTodos = [...completedTodos];
+    reducedCompletedTodos.splice (index);
+    // console.log (reducedCompletedTodos);
+    localStorage.setItem (
+      'completedTodos',
+      JSON.stringify (reducedCompletedTodos)
+    );
+    setCompletedTodos (reducedCompletedTodos);
+  };
+
+  const handleComplete = index => {
+    const date = new Date ();
+    var dd = date.getDate ();
+    var mm = date.getMonth () + 1;
+    var yyyy = date.getFullYear ();
+    var hh = date.getHours ();
+    var minutes = date.getMinutes ();
+    var ss = date.getSeconds ();
+    var finalDate =
+      dd + '-' + mm + '-' + yyyy + ' at ' + hh + ':' + minutes + ':' + ss;
+
+    let filteredTodo = {
+      ...allTodos[index],
+      completedOn: finalDate,
+    };
+
+    // console.log (filteredTodo);
+
+    let updatedCompletedList = [...completedTodos, filteredTodo];
+    console.log (updatedCompletedList);
+    setCompletedTodos (updatedCompletedList);
+    localStorage.setItem (
+      'completedTodos',
+      JSON.stringify (updatedCompletedList)
+    );
+    // console.log (index);
+
+    handleToDoDelete (index);
+  };
+
   return (
     <div className="App">
-      <div className='crud'>
-        <AddList setList = {setList}/>
-        <form onSubmit={handleSubmit}>
-          <table>
-            {
-              lists.map((current) => (
-              updateState === current.id ? <EditList current={current} lists={lists} setList={setList}/> :
-              <tr>
-                <td>{current.name}</td>
-                <td>{current.discription}</td>
-                <td>{current.status}</td>
-                <td>
-                  <IconButton>
-                    <EditIcon className='edit' variant="contained"  onClick={() => handleEdit(current.id)} style={{marginRight:"20px"}} fontSize='small'/>
-                  </IconButton>
-                  <IconButton>
-                    <DeleteIcon color='red' className='delete' variant='contained' onClick={() => handleDelete(current.id)}/>
-                  </IconButton>
-                  
-                </td>
-              </tr>
-              ))
-            }
-          </table>
-        </form>
+      <h1>My Todos</h1>
+
+      <div className="todo-wrapper">
+
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title:</label>
+            <input
+              type="text"
+              value={newTodoTitle}
+              onChange={e => setNewTodoTitle (e.target.value)}
+              placeholder="What's the title of your To Do?"
+            />
+          </div>
+          <div className="todo-input-item">
+            <label>Description:</label>
+            <input
+              type="text"
+              value={newDescription}
+              onChange={e => setNewDescription (e.target.value)}
+              placeholder="What's the description of your To Do?"
+            />
+          </div>
+          <div className="todo-input-item">
+            <AddCircleOutlineIcon fontSize='small'
+              className="primary-btn"
+              type="button"
+              onClick={handleAddNewToDo}
+            />
+          </div>
+        </div>
+        <div className="btn-area">
+          <EventAvailableIcon fontSize='small'
+            className={`secondaryBtn ${isCompletedScreen === false && 'active'}`}
+            onClick={() => setIsCompletedScreen (false)}
+          />
+          <CheckCircleIcon fontSize='small'
+            className={`secondaryBtn ${isCompletedScreen === true && 'active'}`}
+            onClick={() => setIsCompletedScreen (true)}
+          />
+        </div>
+        <div className="todo-list">
+
+          {isCompletedScreen === false &&
+            allTodos.map ((item, index) => (
+              <div className="todo-list-item" key={index}>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+
+                </div>
+                <div style={{display:"flex",flexWrap:"wrap",justifyContent:"center",alignItems:"center"}}>
+                  <DeleteIcon fontSize='small'
+                    title="Delete?"
+                    className="icon"
+                    onClick={() => handleToDoDelete (index)}
+                  />
+                  <AddTaskIcon
+                    title="Completed?"
+                    className=" check-icon"
+                    onClick={() => handleComplete (index)}
+                  />
+                </div>
+              </div>
+            ))}
+
+          {isCompletedScreen === true &&
+            completedTodos.map ((item, index) => (
+              <div className="todo-list-item" key={index}>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                  <p> <i>Completed at: {item.completedOn}</i></p>
+                </div>
+                <div>
+                  <DeleteIcon
+                    className="icon"
+                    onClick={() => handleCompletedTodoDelete (index)}
+                  />
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
-  function handleEdit(id) {
-    setUpdateState(id)
-  }
-  
-  function handleDelete(id) {
-    const newlist = lists.filter((li) => li.id !== id)
-    setList(newlist)
-  }
-  
-  function handleSubmit(event) {
-    event.preventDefault()
-    const name = event.target.elements.name.value
-    const discription = event.target.elements.discription.value
-    const status = event.target.elements.status.value
-    const newlist = lists.map((li) => (
-        li.id === updateState ? {...li, name:name, price:discription, status:status} : li
-    ))
-  
-    setList(newlist)
-    setUpdateState(-1)
-  }
 }
 
-
-function EditList({current, lists, setList}) {
-function handInputname(event) {
-  const value = event.target.value;
-  const newlist = lists.map((li) => (
-      li.id === current.id ? {...li, name :value} : li
-  ))
-
-  setList(newlist)
-}
-
-function handInputprice(event) {
-  const value = event.target.value;
-  const newlist = lists.map((li) => (
-      li.id === current.id ? {...li, price :value} : li
-  ))
-
-  setList(newlist)
-}
-function handStatus(event) {
-  const value = event.target.value;
-  const newlist = lists.map((li) => (
-      li.id === current.id ? {...li, status : value} : li
-  ))
-
-  setList(newlist)
-}
-return(
-  <tr>
-      <td><input type="text" onChange={handInputname} name='name' value={current.name}/></td>
-      <td><input type="text" onChange={handInputprice} name='discription' value={current.discription}/></td>
-      <td><select name="status">
-        <option  onChange={handStatus} name='status' value={current.status}>Completed</option>
-        <option  onChange={handStatus} name='status' value={current.status}>Not Completed</option>
-      </select></td>
-      <td><button type='submit'>Update</button></td>
-  </tr>
-)
-}
-
-function AddList({setList}) {
-const nameRef = useRef()
-const discriptionRef = useRef()
-const statusRef = useRef()
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const name = event.target.elements.name.value;
-  const discription = event.target.elements.discription.value;
-  const status = event.target.elements.status.value; 
-  const newlist = {
-      id: 3,
-      name,
-      discription,
-      status
-  }
-  setList((prevList)=> {
-      return prevList.concat(newlist)
-  })
-  nameRef.current.value = ""
-  discriptionRef.current.value = ""
-  statusRef.current.value = ""
-}
-return(
-  <form className='addForm' onSubmit={handleSubmit} style={{display:"flex",flexWrap:"wrap",flexDirection:"column",justifyContent:"space-between"}}>
-      <input type="text" name="name" placeholder="Enter Name" ref={nameRef}/><br/>
-      <input type="text" name="discription" placeholder="discription" ref={discriptionRef}/><br/>
-      <select name="status">
-        <option  type="text" name='status' ref={statusRef}>Completed</option>
-        <option  type="text" name='status' ref={statusRef}>Not Completed</option>
-      </select><br/>
-      <button type="submit">Add</button>
-  </form>
-)
-}
 
 export default App;
